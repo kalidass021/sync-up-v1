4c61ee76115ade2ccf1f5fe6dbe483c325dc77ac
Kalidass <kalidass.k021@gmail.com>
2024-12-04 18:05:52 +0530

    install and config shacn ui

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
