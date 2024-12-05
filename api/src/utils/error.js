a6c3c4d92dc3ea5ffe32512c3c460106da25891e
Kalidass <kalidass.k021@gmail.com>
2024-12-05 15:34:38 +0530

    develop function to handle custom errors

---
+++ api/src/utils/error.js
@@ -0,0 +1,11 @@
+// function to handle the custom errors
+// when we don't have and error in the system but, we want to add the error
+
+const error = (statusCode, message) => {
+  const err = new Error();
+  err.statusCode = statusCode;
+  err.message = message;
+  return err;
+};
+
+export default error;
