7836f8fa6e0e4ebc7058be8f25e0fc893163b8e6
Kalidass <kalidass.k021@gmail.com>
2024-12-12 23:04:09 +0530

    wrap cloudinary.config inside the try/catch

--- api/src/config/cloudinary.js
+++ api/src/config/cloudinary.js
@@ -2,11 +2,15 @@ import { v2 as cloudinary } from 'cloudinary';
 
 // configure cloudinary
 const cloudinaryConfig = () => {
-  cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-  });
+  try {
+    cloudinary.config({
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
+    });
+  } catch (err) {
+    console.error(`Error while configuring cloudinary ${err.message}`);
+  }
 };
 
 export default cloudinaryConfig;
