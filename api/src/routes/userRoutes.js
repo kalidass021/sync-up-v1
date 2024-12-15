fdada4f148cf70c5a860c3a8e3aae4d690fdead7
Kalidass <kalidass.k021@gmail.com>
2024-12-15 19:14:06 +0530

    add /profile in get user profile route

--- api/src/routes/userRoutes.js
+++ api/src/routes/userRoutes.js
@@ -1,13 +1,16 @@
-import  {Router} from 'express';
-import { getUserProfile, followOrUnfollowUser } from '../controllers/userController.js';
+import { Router } from 'express';
+import {
+  getUserProfile,
+  followOrUnfollowUser,
+} from '../controllers/userController.js';
 import auth from '../middlewares/auth.js';
 import checkId from '../middlewares/checkId.js';
-import {MONGO_ID_REGEX as idRegex} from '../utils/constants.js';
+import { MONGO_ID_REGEX as idRegex } from '../utils/constants.js';
 
 const router = Router();
 
 // private routes
-router.get('/:username', auth, getUserProfile);
+router.get('/:username/profile', auth, getUserProfile);
 router.post(`/:id${idRegex}/follow`, auth, checkId, followOrUnfollowUser);
 
 export default router;
