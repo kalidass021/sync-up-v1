b3e75a766636d0593decc14ce6840d582d8ea6af
Kalidass <kalidass.k021@gmail.com>
2024-12-16 11:57:12 +0530

    creat user api slice and rtk query setup

--- ui/src/config/constants.js
+++ ui/src/config/constants.js
@@ -6,5 +6,6 @@ const BASE_API_URL =
 export const BASE_URL = BASE_API_URL;
 export const AUTH_URL = `${BASE_URL}/api/v1/auth`;
 export const POST_URL = `${BASE_URL}/api/v1/posts`;
+export const USER_URL = `${BASE_URL}/api/v1/users`;
 
 export const CLOUDINARY_URL = import.meta.env.VITE_CLOUDINARY_BASE_URL;
---
+++ ui/src/redux/api/userApiSlice.js
@@ -0,0 +1,37 @@
+import { apiSlice } from './apiSlice';
+import { USER_URL } from '../../config/constants';
+
+export const userApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getUserProfile: builder.query({
+      query: (username) => `${USER_URL}/${username}/profile`,
+    }),
+
+    followOrUnfollowUser: builder.mutation({
+      // id is targer user id
+      query: (id) => ({
+        url: `${USER_URL}/${id}/follow`,
+        method: 'POST',
+      }),
+    }),
+
+    getSuggestedUsers: builder.query({
+      query: () => `${USER_URL}/suggested`,
+    }),
+
+    updateUserProfile: builder.mutation({
+      query: ({ username, updatedUser }) => ({
+        url: `${USER_URL}/${username}/profile`,
+        method: 'PUT',
+        body: updatedUser,
+      }),
+    }),
+  }),
+});
+
+export const {
+  useGetUserProfileQuery,
+  useFollowOrUnfollowUserMutation,
+  useGetSuggestedUsersQuery,
+  useUpdateUserProfileMutation,
+} = userApiSlice;
