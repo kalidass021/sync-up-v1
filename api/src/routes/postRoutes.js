77442919c6c826f3e9951c6975a78da83d4bb65e
Kalidass <kalidass.k021@gmail.com>
2024-12-21 18:58:37 +0530

    reorder the /:username route to avoid it's get execuuted ror /infinite and /search routes

--- api/src/routes/postRoutes.js
+++ api/src/routes/postRoutes.js
@@ -19,14 +19,16 @@ import { MONGO_ID_REGEX as idRegex } from '../utils/constants.js';
 
 const router = Router();
 
+const regex = /^[a-zA-Z0-9_]+$/;
+
 // private routes
 router.post('/', auth, createPost);
 router.get(`/:id${idRegex}`, auth, checkId, getSpecificPost);
 router.get('/recents', auth, getRecentPosts);
-router.get('/:username', auth, getUserPosts);
 router.get('/', auth, getPostsByIds);
 router.get('/infinite', auth, getInfinitePosts);
 router.get('/search', auth, searchPosts);
+router.get('/:username', auth, getUserPosts); // avoid /:username get executed for /infinite and /search
 router.put(`/:id${idRegex}`, auth, checkId, updatePost);
 router.delete(`/:id${idRegex}`, auth, checkId, deletePost);
 router.post(`/:id${idRegex}/like`, auth, checkId, likeOrUnlikePost);
