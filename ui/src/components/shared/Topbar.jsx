b425a3c84d71a51d4c0ac8095a4b292d1d23db4b
Kalidass <kalidass.k021@gmail.com>
2024-12-26 17:28:43 +0530

    remove ununsed useSelector

--- ui/src/components/shared/Topbar.jsx
+++ ui/src/components/shared/Topbar.jsx
@@ -1,5 +1,5 @@
 import { Link, useNavigate } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import toast from 'react-hot-toast';
 import {
   useGetCurrentUserProfileQuery,
