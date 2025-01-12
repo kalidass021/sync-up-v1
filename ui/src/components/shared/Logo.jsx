d1cd8c600648324e6f9392a5088e4fe8b9f3cc99
Kalidass <kalidass.k021@gmail.com>
2025-01-12 22:18:38 +0530

    update logo

--- ui/src/components/shared/Logo.jsx
+++ ui/src/components/shared/Logo.jsx
@@ -1,7 +1,10 @@
+import logo from '../../assets/images/logo.svg';
+
 const Logo = () => {
   return (
-    <div>
-      <h1 className='font-mono font-semibold text-4xl text-light-3'>sync up</h1>
+    <div className='flex items-center'>
+      <img src={logo} alt='logo' className='mr-2' />
+      <h1 className='font-mono font-semibold text-4xl text-light-3 mb-1'>Sync up</h1>
     </div>
   );
 };
