23291775d3a278854b6729381f6eafb4071bdfa9
Kalidass <kalidass.k021@gmail.com>
2025-01-13 13:23:12 +0530

    remove useGetInfinitePostsQuery from postApiSlice since it it not used in the explore page

--- ui/src/redux/api/postApiSlice.js
+++ ui/src/redux/api/postApiSlice.js
@@ -91,7 +91,6 @@ export const {
   useGetUserPostsQuery,
   useGetPostsByIdsQuery,
   useLazyGetInfinitePostsQuery,
-  useGetInfinitePostsQuery,
   useLazySearchPostsQuery,
   useSearchPostsQuery,
   useUpdatePostMutation,
