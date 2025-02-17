372f459d603de84c44053d4b545e1067472d5df5
Kalidass <kalidass.k021@gmail.com>
2025-02-17 23:39:14 +0530

    remove password lenght validation from signin form

--- ui/src/pages/auth/forms/SigninForm.jsx
+++ ui/src/pages/auth/forms/SigninForm.jsx
@@ -100,10 +100,6 @@ const SigninForm = () => {
             className='text-black'
             {...register('password', {
               required: 'Password is required',
-              minLength: {
-                value: 8,
-                message: 'Password must be at least 8 characters',
-              },
             })}
           />
           {errors.password && (
