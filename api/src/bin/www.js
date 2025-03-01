c3ea40cfe08c93df02f1a65e860b6e064ba7e959
Kalidass <kalidass.k021@gmail.com>
2025-03-01 21:38:31 +0530

    test by adding async/await for startServer function

--- api/src/bin/www.js
+++ api/src/bin/www.js
@@ -5,7 +5,7 @@ import dbConnect from '../config/dbConnect';
     // connect to db
     await dbConnect();
     const { default: startServer } = await import('../server.js');
-    startServer(); // attempt to start the server
+    await startServer(); // attempt to start the server
   } catch (err) {
     console.error(`Startup Error: ${err}`);
     process.exit(1); // exit with failure
