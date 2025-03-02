bdf4cb67e235cf1b2a2149fa1a690ae7bb998195
Kalidass <kalidass.k021@gmail.com>
2025-03-02 18:48:11 +0530

    revert the change

--- api/src/bin/www.js
+++ api/src/bin/www.js
@@ -1,10 +1,11 @@
-import dbConnect from '../config/dbConnect.js';
+import dbConnect from '../config/dbConnect';
+import startServer from '../server';
 
 (async () => {
   try {
     // connect to db
     await dbConnect();
-    const { default: startServer } = await import('../server.js');
+    // const { default: startServer } = await import('../server.js');
     startServer(); // attempt to start the server
   } catch (err) {
     console.error(`Startup Error: ${err}`);
