2e0d7aca87a45db225dc5bc2567fbc334b84d5b7
Kalidass <kalidass.k021@gmail.com>
2025-03-13 23:31:27 +0530

    use router.route to make code cleaner in notificationRoutes

--- api/src/routes/notificationRoutes.js
+++ api/src/routes/notificationRoutes.js
@@ -1,10 +1,14 @@
 import { Router } from 'express';
-import { getNotifications, deleteNotifications } from '../controllers/notificationController';
+import {
+  getNotifications,
+  deleteNotifications,
+} from '../controllers/notificationController';
 import { auth } from '../middlewares';
 
 const router = Router();
 
-router.get('/', auth, getNotifications);
-router.delete('/', auth, deleteNotifications);
+router.route('/')
+    .get(auth, getNotifications)
+    .delete(auth, deleteNotifications);
 
 export default router;
