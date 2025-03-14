5f0c948905480b6e03b3ac61aea15d3de49f6987
Kalidass <kalidass.k021@gmail.com>
2025-03-14 15:55:19 +0530

    add comments

--- api/src/config/dbConnect.js
+++ api/src/config/dbConnect.js
@@ -4,13 +4,16 @@ const dbConnect = async () => {
   // get default connection
   const db = mongoose.connection;
 
-  db.on('error', () => {
-    console.error(`Error while connnecting to Mongo DB: ${err.message}`);
+  // handle errors after initial connection
+  db.on('error', (err) => {
+    console.error(`Mongo DB runtime error: ${err.message}`);
   });
 
   db.once('open', () => {
     console.info('Connected to Mongo DB 👍');
   });
+
+  // connect to mongo db
   await mongoose.connect(process.env.MONGO_URI);
 };
 
