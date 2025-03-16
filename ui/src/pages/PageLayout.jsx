c313862f718610202681fa43d5697ee1526aaf58
Kalidass <kalidass.k021@gmail.com>
2025-03-16 18:01:08 +0530

    show back to online toast only when app transitions from offline to online

--- ui/src/pages/PageLayout.jsx
+++ ui/src/pages/PageLayout.jsx
@@ -1,20 +1,36 @@
+import { useCallback, useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
+import { toast } from 'sonner';
 import { Outlet, Navigate } from 'react-router-dom';
 import Topbar from '../components/shared/Topbar';
 import LeftSidebar from '../components/shared/LeftSidebar';
 import Bottombar from '../components/shared/Bottombar';
 import { useOnlineStatus } from '../hooks';
-import { toast } from 'sonner';
 
 const PageLayout = () => {
   const { userInfo } = useSelector((state) => state.auth);
   const online = useOnlineStatus();
 
-  if (!online) {
-    toast.error(`You're offline! Check your internet connection.`);
-  } else {
-    toast.success(`Connection restored. Let's sync things up!`);
-  }
+  // show online toast only when the app transitions from offline to online
+  // Ref to track the previous online status
+  const prevOnlineStatus = useRef(online);
+
+  const handleOnlineStatusChange = useCallback(() => {
+    if (prevOnlineStatus.current !== online) {
+      if (!online) {
+        toast.error(`You're offline! check your internet connection`);
+      } else {
+        toast.success(`Connection restored. Let's sync things up!`);
+      }
+
+      // update the previous status
+      prevOnlineStatus.current = online;
+    }
+  }, [online]);
+
+  useEffect(() => {
+    handleOnlineStatusChange();
+  }, [handleOnlineStatusChange]);
 
   if (!userInfo) {
     return <Navigate to='/signin' replace />;
