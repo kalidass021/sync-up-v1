c2a3f7559dc0e641fd3f4df2b070da79543c2e78
Kalidass <kalidass.k021@gmail.com>
2025-04-17 23:15:45 +0530

    add speed insights and analytics components vercel to track performance and user interactions

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,6 +1,8 @@
 import { Outlet } from 'react-router-dom';
 import { Toaster } from 'sonner';
 import './index.css';
+import { SpeedInsights } from '@vercel/speed-insights/react'; // for monitoring and reporting the performance
+import { Analytics } from '@vercel/analytics/react'; // for tracking the user interactions
 
 const App = () => {
   return (
@@ -9,6 +11,8 @@ const App = () => {
       <main className='flex h-screen'>
         <Outlet />
       </main>
+      <SpeedInsights />
+      <Analytics />
     </>
   );
 };
