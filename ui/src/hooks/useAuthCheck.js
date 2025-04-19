16c4f2e8412cddea167ce7b5b2627501725ebbbf
Kalidass <kalidass.k021@gmail.com>
2025-04-19 15:58:57 +0530

    wrap handle signout function with useCallback

--- ui/src/hooks/useAuthCheck.js
+++ ui/src/hooks/useAuthCheck.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { toast } from 'sonner';
 import {
@@ -12,27 +12,29 @@ const useAuthCheck = () => {
   const authResponse = useAuthCheckQuery();
   const dispatch = useDispatch();
   const [signoutApiCall] = useSignoutMutation();
-  
+
+  const handleSignout = useCallback(async () => {
+    try {
+      await signoutApiCall().unwrap();
+      dispatch(signout());
+      toast.info('Authentication cookie missing. Please sign in again');
+      setIsAuthorized(false);
+    } catch (err) {
+      console.error(`Error while signout ${err?.data?.message}`);
+      toast.error(err?.data?.message);
+    }
+  }, [dispatch, signoutApiCall]);
+
   useEffect(() => {
     const isUnauthorizedError =
       authResponse?.error?.data?.message === 'Unauthorized: No token provided';
 
-    if (!isUnauthorizedError) return;
-
+      console.log(isUnauthorizedError, 'isUnauthorizedError');
     // if the error is unauthorized error (cookie not present) signout the user
-    const handleSignout = async () => {
-      try {
-        await signoutApiCall().unwrap();
-        dispatch(signout());
-        toast.info('Authentication cookie missing. Please sign in again');
-        setIsAuthorized(false);
-      } catch (err) {
-        console.error(`Error while signout ${err?.data?.message}`);
-        toast.error(err?.data?.message);
-      }
-    };
-    handleSignout();
-  }, [authResponse, dispatch]);
+    if (isUnauthorizedError) {
+      handleSignout();
+    }
+  }, [authResponse, handleSignout]);
 
   return isAuthorized;
 };
