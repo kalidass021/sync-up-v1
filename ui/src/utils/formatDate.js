bd624296452d77c48974e78ca5c7e9385b332371
Kalidass <kalidass.k021@gmail.com>
2025-06-27 00:13:00 +0530

    update format date logic

--- ui/src/utils/formatDate.js
+++ ui/src/utils/formatDate.js
@@ -2,31 +2,30 @@ const formatDate = (createdAt) => {
   const createdDate = new Date(createdAt);
   const now = new Date();
 
-  // check if the createAt is valid date
   if (isNaN(createdDate.getTime())) {
-    return 'Invalid date';
+    return 'Invalid Date';
   }
 
-  const seconds = Math.floor((now - createdDate) / 1000);
-  const minutes = Math.floor(seconds / 60);
-  const hours = Math.floor(minutes / 60);
-  const days = Math.floor(hours / 24);
-  const months = Math.floor(days / 30);
-  const years = Math.floor(days / 365);
+  const diffInSeconds = Math.floor((now - createdDate) / 1000);
 
-  if (seconds < 60) {
-    return seconds === 1 ? '1 second ago' : `${seconds} seconds ago`;
-  } else if (minutes < 60) {
-    return minutes === 1 ? '1 minute ago' : `${minutes} minutes ago`;
-  } else if (hours < 24) {
-    return hours === 1 ? '1 hour ago' : `${hours} hours ago`;
-  } else if (days < 30) {
-    return days === 1 ? '1 day ago' : `${days} days ago`;
-  } else if (months < 12) {
-    return months === 1 ? '1 month ago' : `${months} months ago`;
-  } else {
-    return years === 1 ? '1 year ago' : `${years} years ago`;
+  const units = [
+    {label: 'year', seconds: 365 * 24 * 60 * 60},
+    {label: 'month', seconds: 30 * 24 * 60 * 60},
+    {label: 'day', seconds: 24 * 60 * 60},
+    {label: 'hour', seconds: 60 * 60},
+    {label: 'minute', seconds: 60},
+    {label: 'second', seconds: 1},
+  ]
+
+  for (let unit of units) {
+    const value = Math.floor(diffInSeconds / unit.seconds);
+
+    if (value >= 1) {
+      return `${value} ${unit.label}${value > 1 && 's'} ago`;
+    }
   }
-};
+
+  return 'just now';
+}
 
 export default formatDate;
