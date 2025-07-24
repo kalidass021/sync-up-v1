5977b54fb8c0f5f41b86d696769833e3ec7d00f6
Kalidass <kalidass.k021@gmail.com>
2025-07-24 23:05:16 +0530

    correct some css in user card loader

--- ui/src/components/skeleton/user/UserCardLoader.jsx
+++ ui/src/components/skeleton/user/UserCardLoader.jsx
@@ -6,17 +6,17 @@ const UserCardLoader = ({
 }) => {
   return (
     <div
-      className={`user-card flex items-center justify-between mt-2 ${containerWidth} h-[236px]`}
+      className={`user-card flex items-center justify-between ${containerWidth} h-auto`}
     >
       {/* Profile Image + User Info */}
-      <div className='flex flex-col items-center gap-3'>
+      <div className='flex flex-col items-center gap-3 mt-[5px]'>
         <Skeleton className='w-14 h-14 rounded-full bg-dark-4' />
         <Skeleton className='h-4 w-[120px] rounded-full bg-dark-4' />
         <Skeleton className='h-4 w-[100px] rounded-full bg-dark-4' />
       </div>
 
       {/* Follow Button Placeholder */}
-      <Skeleton className={`h-8 ${buttonWidth} rounded-md bg-dark-4`} />
+      <Skeleton className={`h-8 ${buttonWidth} rounded-md bg-dark-4 mb-[5px]`} />
     </div>
   );
 };
