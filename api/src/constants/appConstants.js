bdf225d4ad5be832158f30851dcb85e806f6d211
Kalidass <kalidass.k021@gmail.com>
2025-07-25 23:56:37 +0530

    refactor post routes

--- api/src/constants/appConstants.js
+++ api/src/constants/appConstants.js
@@ -1,3 +1,6 @@
+// mongo db object id pattern regex
+export const MONGO_ID_REGEX = '([0-9a-fA-F]{24})';
+
 export const STATUS_CODES = {
   OK: 200,
   Created: 201,
@@ -16,12 +19,12 @@ export const STATUS_CODES = {
 };
 
 export const ROUTES = {
-    ROOT: '/',
-    AUTH: '/v1/auth',
-    USERS: '/v1/users',
-    POSTS: '/v1/posts',
-    NOTIFICATIONS: '/v1/notifications',
-}
+  ROOT: '/',
+  AUTH: '/v1/auth',
+  USERS: '/v1/users',
+  POSTS: '/v1/posts',
+  NOTIFICATIONS: '/v1/notifications',
+};
 
 export const AUTH_ENDPOINTS = {
   AUTH_CHECK: '/',
@@ -29,8 +32,23 @@ export const AUTH_ENDPOINTS = {
   SIGNIN: '/signin',
   SIGNOUT: '/signout',
   GET_CURRENT_USER_PROFILE: '/profile',
-}
+};
 
 export const NOTIFICATION_ENDPOINTS = {
   PROCESS_NOTIFICATIONS: '/',
-}
+};
+
+export const POST_ENDPOINTS = {
+  CREATE_POST: '/',
+  GET_SPECIFIC_POST: `/:id${MONGO_ID_REGEX}`,
+  GET_RECENT_POSTS: '/recents',
+  GET_POSTS_BY_IDS: '/',
+  GET_INFINITE_POSTS: '/infinite',
+  SEARCH_POSTS: '/search',
+  FETCH_MEME_OF_THE_DAY: '/meme',
+  GET_USER_POSTS: '/:username',
+  UPDATE_POST: `/:id${MONGO_ID_REGEX}`,
+  DELETE_POST: `/:id${MONGO_ID_REGEX}`,
+  LIKE_OR_UNLIKE_POST: `/:id${MONGO_ID_REGEX}/like`,
+  SAVE_OR_UNSAVE_POST: `/:id${MONGO_ID_REGEX}/save`,
+};
--- api/src/routes/postRoutes.js
+++ api/src/routes/postRoutes.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { POST_ENDPOINTS } from '../constants/appConstants';
 import * as postController from '../controllers/postController';
 // middlewares
 import { auth, checkId } from '../middlewares';
@@ -9,17 +10,32 @@ const router = Router();
 // apply auth middleware to all routes in the router
 router.use(auth);
 // private routes
-router.post('/', postController.createPost);
-router.get(`/:id${idRegex}`, checkId, postController.getSpecificPost);
-router.get('/recents', postController.getRecentPosts);
-router.get('/', postController.getPostsByIds);
-router.get('/infinite', postController.getInfinitePosts);
-router.get('/search', postController.searchPosts);
-router.get('/meme', postController.fetchMemeOfTheDay);
-router.get('/:username', postController.getUserPosts); // avoid /:username get executed for /infinite and /search
-router.put(`/:id${idRegex}`, checkId, postController.updatePost);
-router.delete(`/:id${idRegex}`, checkId, postController.deletePost);
-router.post(`/:id${idRegex}/like`, checkId, postController.likeOrUnlikePost);
-router.post(`/:id${idRegex}/save`, checkId, postController.saveOrUnsavePost);
+router.post(POST_ENDPOINTS.CREATE_POST, postController.createPost);
+router.get(
+  POST_ENDPOINTS.GET_SPECIFIC_POST,
+  checkId,
+  postController.getSpecificPost
+);
+router.get(POST_ENDPOINTS.GET_RECENT_POSTS, postController.getRecentPosts);
+router.get(POST_ENDPOINTS.GET_POSTS_BY_IDS, postController.getPostsByIds);
+router.get(POST_ENDPOINTS.GET_INFINITE_POSTS, postController.getInfinitePosts);
+router.get(POST_ENDPOINTS.SEARCH_POSTS, postController.searchPosts);
+router.get(
+  POST_ENDPOINTS.FETCH_MEME_OF_THE_DAY,
+  postController.fetchMemeOfTheDay
+);
+router.get(POST_ENDPOINTS.GET_USER_POSTS, postController.getUserPosts); // avoid /:username get executed for /infinite and /search
+router.put(POST_ENDPOINTS.UPDATE_POST, checkId, postController.updatePost);
+router.delete(POST_ENDPOINTS.DELETE_POST, checkId, postController.deletePost);
+router.post(
+  POST_ENDPOINTS.LIKE_OR_UNLIKE_POST,
+  checkId,
+  postController.likeOrUnlikePost
+);
+router.post(
+  POST_ENDPOINTS.SAVE_OR_UNSAVE_POST,
+  checkId,
+  postController.saveOrUnsavePost
+);
 
 export default router;
