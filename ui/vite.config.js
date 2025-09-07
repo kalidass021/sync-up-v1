dc9c4affd90e56871743fffb55a8f31dfec750ba
Kalidass <kalidass.k021@gmail.com>
2025-09-07 15:15:17 +0530

    implement preload functionality

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -2,13 +2,14 @@ import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import preload from 'vite-plugin-preload';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), preload()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
