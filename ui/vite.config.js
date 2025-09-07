6a1d9f62718527e88141834a5cbbb735ed7e66c1
Kalidass <kalidass.k021@gmail.com>
2025-09-07 15:28:47 +0530

    install and config bundle analyzer

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -3,13 +3,14 @@ import path, { dirname } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import preload from 'vite-plugin-preload';
+import { analyzer } from 'vite-bundle-analyzer';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), preload()],
+  plugins: [react(), preload(), analyzer()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
