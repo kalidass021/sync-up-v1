e864a273fdb74d8cd1f183523f8b6652c3e3b4d8
Kalidass <kalidass.k021@gmail.com>
2026-01-01 15:36:19 +0530

    refetch all subscribed queries after application window/tab regains the focus and regaining a network connection

--- ui/src/redux/api/apiSlice.js
+++ ui/src/redux/api/apiSlice.js
@@ -5,6 +5,8 @@ const baseQuery = fetchBaseQuery({ baseUrl: API_BASE_URL, credentials: 'include'
 
 export const apiSlice = createApi({
   baseQuery,
+  refetchOnFocus: true, // refetch all subscribed queries after the application window/tab regains the focus
+  refetchOnReconnect: true, // refetch all subscribed queries after regaining a network connection
   tagTypes: ['User', 'Post', 'UserProfile', 'SuggestedUsers', 'AllUsers'],
   endpoints: () => ({}),
 });
