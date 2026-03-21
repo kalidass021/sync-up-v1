7a40f797b55cf038720694e15647327150dc7865
Kalidass <kalidass.k021@gmail.com>
2026-03-21 15:12:56 +0530

    use status code from the constants in the apiStatus.js

--- api/src/utils/apiStatus.js
+++ api/src/utils/apiStatus.js
@@ -1,8 +1,9 @@
 import sendResponse from '../core/sendResponse';
+import { STATUS_CODES } from '../constants';
 import { API_STATUS } from '../constants';
 
 const apiStatus = (req, res) => {
-  return sendResponse(200, API_STATUS.WORKING, {
+  return sendResponse(STATUS_CODES.OK, API_STATUS.WORKING, {
     environment: process.env.NODE_ENV,
   })(req, res);
 };
