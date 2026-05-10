c1129f694630d278186d0399f6d5adb0723452b7
Kalidass <kalidass.k021@gmail.com>
2026-05-10 13:03:57 +0530

    Fix: 1 yearfalse ago bug

--- ui/src/utils/formatDate.js
+++ ui/src/utils/formatDate.js
@@ -9,23 +9,23 @@ const formatDate = (createdAt) => {
   const diffInSeconds = Math.floor((now - createdDate) / 1000);
 
   const units = [
-    {label: 'year', seconds: 365 * 24 * 60 * 60},
-    {label: 'month', seconds: 30 * 24 * 60 * 60},
-    {label: 'day', seconds: 24 * 60 * 60},
-    {label: 'hour', seconds: 60 * 60},
-    {label: 'minute', seconds: 60},
-    {label: 'second', seconds: 1},
-  ]
+    { label: 'year', seconds: 365 * 24 * 60 * 60 },
+    { label: 'month', seconds: 30 * 24 * 60 * 60 },
+    { label: 'day', seconds: 24 * 60 * 60 },
+    { label: 'hour', seconds: 60 * 60 },
+    { label: 'minute', seconds: 60 },
+    { label: 'second', seconds: 1 },
+  ];
 
   for (let unit of units) {
     const value = Math.floor(diffInSeconds / unit.seconds);
 
     if (value >= 1) {
-      return `${value} ${unit.label}${value > 1 && 's'} ago`;
+      return `${value} ${unit.label}${value > 1 ? 's' : ''} ago`;
     }
   }
 
   return 'just now';
-}
+};
 
 export default formatDate;
